0937507c429ced5d9cba7d1b74d480cab0f21716
Tiardo Wiechmann <tiardo.niklas.wiechmann@student.hu-berlin.de>
2026-08-02 19:51:57 +0200

    Add more methods to bSTree.js

--- bSTree.js
+++ bSTree.js
@@ -16,8 +16,23 @@ class Tree {
         else {
             array.sort();
         }
-        const newArr = removeDuplicates(array);
+        const newArr = Tree.#removeDuplicates(array);
         this.root = Tree.#buildTree(newArr, 0, newArr.length-1);
+    }
+
+    static #removeDuplicates(sortedArr) {
+        if(!sortedArr) {
+            return [];
+        }
+        let newArr = [sortedArr[0]];
+        for (let i=1; i<sortedArr.length; i++) {
+            if (sortedArr[i-1] === sortedArr[i]){
+                continue;
+            }
+            newArr.push(sortedArr[i]);
+        }
+        
+        return newArr;
     }
 
     /**
@@ -46,7 +61,6 @@ class Tree {
     }
 
     static #includes(root, value) {
-        console.log(root);
         if (root === null) {
             return false;
         }
@@ -57,6 +71,85 @@ class Tree {
         return Tree.#includes(root.left, value) || 
                Tree.#includes(root.right, value);
     }
+
+
+    /**
+     * Accepts a value and inserts a new node with that value into the tree 
+     */
+    insert(value) {
+        this.root = Tree.#insert(this.root, value);
+    }
+
+    static #insert(root, value) {
+        if(root === null) {
+            return new Node(value);
+        }
+        else if(root.data < value) {
+            root.right = Tree.#insert(root.right, value);
+        }
+        else if (value < root.data) {
+            root.left = Tree.#insert(root.left, value);
+        }
+        return root;
+    }
+
+
+    /**
+     * Accepts a value and removes it from the tree. 
+     */
+    deleteItem(value) {
+        this.root = Tree.#deleteItem(this.root, value);
+    }
+
+    static #deleteItem(root, value) {
+        if(root === null) {
+            return root;
+        }
+
+        if (value < root.data) {
+            root.left = Tree.#deleteItem(root.left, value);
+        }
+        else if (root.data < value) {
+            root.right = Tree.#deleteItem(root.right, value)
+        }
+        // if root.data == value
+        else {
+            if(root.left === null) {
+                return root.right;
+            }
+            else if(root.right === null) {
+                return root.left;
+            }
+            else {
+                const succ = Tree.#getSuccessor(root);
+                root.data = succ.data;
+                root.right = Tree.#deleteItem(root.right, succ.data);
+            }
+        }
+        return root;
+    }
+
+    static #getSuccessor(curr){
+        curr = curr.right;
+        while(curr != null && curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+
+    /**
+     * Accepts a callback function as its parameter. 
+     * levelOrderForEach() should traverse the tree in 
+     * breadth-first level order and call the callback 
+     * on each value as it traverses, passing each value 
+     * (not the nodes) as an argument.
+     */
+    levelOrderForEach(callback) {
+        
+    }
+
+
 
     /**
      * Visualizes BST
@@ -70,24 +163,6 @@ class Tree {
         console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
         Tree.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
       }
-      
-    
-}
-
-
-function removeDuplicates(sortedArr) {
-    if(!sortedArr) {
-        return [];
-    }
-    let newArr = [sortedArr[0]];
-    for (let i=1; i<sortedArr.length; i++) {
-        if (sortedArr[i-1] === sortedArr[i]){
-            continue;
-        }
-        newArr.push(sortedArr[i]);
-    }
-    
-    return newArr;
 }
 
 
@@ -95,3 +170,8 @@ let arr = [1, 2, 2, 0, 5, 4];
 const tree = new Tree(arr);
 Tree.prettyPrint(tree.root);
 console.log(tree.includes(1));
+tree.insert(3);
+Tree.prettyPrint(tree.root);
+tree.deleteItem(tree.root, 2);
+Tree.prettyPrint(tree.root);
+
